d5d4647c2fc14ede07450edbeec545000cd5b92b
Limc <i@limc.me>
2023-05-16 12:19:07 +0800

    feat: mask close to home

--- app/components/mask.tsx
+++ app/components/mask.tsx
@@ -3,30 +3,30 @@ import { ErrorBoundary } from "./error";
 
 import styles from "./mask.module.scss";
 
-import DownloadIcon from "../icons/download.svg";
-import UploadIcon from "../icons/upload.svg";
-import EditIcon from "../icons/edit.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
-import DeleteIcon from "../icons/delete.svg";
-import EyeIcon from "../icons/eye.svg";
 import CopyIcon from "../icons/copy.svg";
+import DeleteIcon from "../icons/delete.svg";
+import DownloadIcon from "../icons/download.svg";
+import EditIcon from "../icons/edit.svg";
+import EyeIcon from "../icons/eye.svg";
+import UploadIcon from "../icons/upload.svg";
 
-import { DEFAULT_MASK_AVATAR, Mask, useMaskStore } from "../store/mask";
-import { ChatMessage, ModelConfig, useChatStore } from "../store";
-import { ROLES } from "../client/api";
-import { Input, List, ListItem, Modal, Popover, Select } from "./ui-lib";
-import { Avatar, AvatarPicker } from "./emoji";
-import Locale, { AllLangs, Lang } from "../locales";
 import { useNavigate } from "react-router-dom";
+import { ROLES } from "../client/api";
+import Locale, { AllLangs, Lang } from "../locales";
+import { ChatMessage, ModelConfig, useChatStore } from "../store";
+import { DEFAULT_MASK_AVATAR, Mask, useMaskStore } from "../store/mask";
+import { Avatar, AvatarPicker } from "./emoji";
+import { Input, List, ListItem, Modal, Popover, Select } from "./ui-lib";
 
-import chatStyle from "./chat.module.scss";
 import { useState } from "react";
-import { downloadAs, readFromFile } from "../utils";
-import { Updater } from "../typing";
-import { ModelConfigList } from "./model-config";
 import { FileName, Path } from "../constant";
 import { BUILTIN_MASK_STORE } from "../masks";
+import { Updater } from "../typing";
+import { downloadAs, readFromFile } from "../utils";
+import chatStyle from "./chat.module.scss";
+import { ModelConfigList } from "./model-config";
 
 export function MaskAvatar(props: { mask: Mask }) {
   return props.mask.avatar !== DEFAULT_MASK_AVATAR ? (
@@ -293,7 +293,7 @@ export function MaskPage() {
               <IconButton
                 icon={<CloseIcon />}
                 bordered
-                onClick={() => navigate(-1)}
+                onClick={() => navigate(Path.Home)}
               />
             </div>
           </div>
